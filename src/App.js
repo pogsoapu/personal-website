0f5792d57bb5ceb1da142ab7b38e5b0a16d6ea54
Sophia Naveed <sophia.naveed@mail.utoronto.ca>
2024-04-01 21:48:46 -0400

    app setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './silly.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          wip!!!!!!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://twitter.com/sopsoapu"
+          target="_blank"
+        >
+          awesome link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
