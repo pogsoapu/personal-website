b71eafcc95122d33cd7b0eca43a60e58f5bf5380
unknown <snrocks2001@gmail.com>
2024-04-01 22:01:55 -0400

    fixing git issue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          wip!!!!!!!!
+          wip!!!!!!!! my personal website
         </p>
         <a
           className="App-link"
